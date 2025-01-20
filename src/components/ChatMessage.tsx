bb987b938b6f1017e67baefaaad3399ec585a0ff
Amine BIZID <amine.bizid@gmail.com>
2025-01-20 11:40:36 +0100

    qq

--- src/components/ChatMessage.tsx
+++ src/components/ChatMessage.tsx
@@ -21,7 +21,7 @@ export const ChatMessage = ({ content, isBot, isStreaming }: ChatMessageProps) =
   return (
     <div
       className={cn(
-        "flex w-full mb-4 animate-fade-in",
+        "flex w-full mb-2 animate-fade-in",
         isBot ? "justify-start" : "justify-end"
       )}
     >
