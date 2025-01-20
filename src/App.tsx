9cd308cebad7e5d1bcde291bc9dcffdaef9035ef
Amine BIZID <amine.bizid@gmail.com>
2025-01-20 09:59:03 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider,  } from "@tanstack/react-query";
+import { QueryClient } from '@tanstack/query-core';
+import { QueryClientProvider,  } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "next-themes";
 import { ThemeToggle } from "@/components/ThemeToggle";
